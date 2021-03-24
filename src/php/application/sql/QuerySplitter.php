af597cac965a46cfa57c6f9069d1a4c8457a7c46
Pawel Gruszka <vespy.pawel.gruszka@gmail.com>
2021-03-25 00:22:17 +0100

    reworked query splitter

--- src/php/application/sql/QuerySplitter.php
+++ src/php/application/sql/QuerySplitter.php
@@ -11,9 +11,80 @@ class QuerySplitter
      */
     public function split($sql)
     {
-        $sql = $this->removeCommentsAndSpaces($sql);
+        // temporarily replace semicolons in quoted strings with a placeholder *ris_db_migrate_semicolon*
+        $sql = preg_replace_callback('/([\'\"])[\s\S]*?\1/', function ($matches) {
+            return str_replace(';', 'ris_db_migrate_semicolon', $matches[0]);
+            // remove single quotes from comments (can only do if line starts with -- otherwise tbl_process --param will get picked up)
+            // so please no single quotes in inline comments or this will fail
+        }, preg_replace_callback('/^ *--.*/', function ($matches) {
+            return '--' . preg_replace('/\'/', '', $matches[0]);
+        }, $sql));
 
-        return array_values(array_filter(explode(PHP_EOL."".PHP_EOL, $sql)));
+        // sql blocks matching patterns (order matters)
+        $patterns = [
+            // find DO .. ; blocks
+            '/^ *do[\s\r\n]+?\$\$[\s\S]+?;?[\s\S]*?\$\$;?/im',
+            // find CREATE ... FUNCTION ... BEGIN ... END
+            '/^ *create[\s\S]+?function[\s\S]+?begin[\s\S]+?end[\s\S]+?\$\$;?/im',
+            // find CREATE ... RULE ... newLine x2 or end of file (create rule has to end with 2 line breaks for this to work) It could be done by recursive regex (?R), but I couldn't get it to work
+            '/^ *create[\n\r\s]+(or[\n\r\s]+replace[\n\r\s]+)?rule[\s\S]+?;[\n\r\s]+([\n\r]|$)/im',
+            // find any other queries until ; char
+            '/^ *(select|insert|drop|update|create|alter|delete|truncate|grant)[\s\S]*?;/im',
+        ];
+        $queries = [];
+        $sqlReplacementSchema = $sql;
+
+        // find query blocks and temporarily replace it with a placeholder *ris_db_migrate_replacement_XX_XX* to then know query order
+        foreach ($patterns as $key => $pattern) {
+            $sqlReplacementSchema = preg_replace_callback($pattern, function ($matches) use (&$queries, $key) {
+                $queries[$key] = $queries[$key] ?? [];
+                $queries[$key][] = $matches[0];
+                return "\n" . 'ris_db_migrate_replacement_' . $key . '_' . (count($queries[$key]) - 1) . "\n";
+            }, $sql);
+            $sql = preg_replace($pattern, '', $sqlReplacementSchema);
+
+        }
+
+        $remainsToProcess = '';
+        $queriesToProcess = [];
+        // iterate placeholders (one per line) and build queries array based on the placeholders
+        foreach (explode(PHP_EOL, $sqlReplacementSchema) as $line) {
+            if (preg_match('/^ris_db_migrate_replacement_([0-9])+_([0-9]+)$/', trim($line), $matches)) {
+                $remainsToProcess = trim($remainsToProcess);
+                if ($remainsToProcess) {
+                    echo 'String didn\'t match any query pattern. Falling back to the old query splitter for the following string:' . "\n" .   $remainsToProcess . "\n";
+                    $queriesToProcess = array_merge($queriesToProcess, $this->splitOld($remainsToProcess));
+                    $remainsToProcess = '';
+                }
+                $queriesToProcess[] = $queries[$matches[1]][$matches[2]];
+            } elseif (!preg_match('/^--/', trim($line))) {
+                $remainsToProcess .= preg_replace('/\s*-{2,}\s*.*<\/?[a-zA-Z0-9_.-]+>/', '', $line) . PHP_EOL;
+            }
+        }
+        $remainsToProcess = trim($remainsToProcess);
+        if ($remainsToProcess) {
+            echo 'String didn\'t match any query pattern. Falling back to the old query splitter for the following string:' . "\n" .   $remainsToProcess . "\n";
+            $queriesToProcess = array_merge($queriesToProcess, $this->splitOld($remainsToProcess));
+        }
+        // trim and remove empty queries
+        $queries = array_values(array_filter(array_map('trim', $queriesToProcess)));
+
+        // restore semicolons
+        foreach ($queries as $key => $query) {
+            $queries[$key] = str_replace('ris_db_migrate_semicolon', ';', $query);
+        }
+        return $queries;
+    }
+
+    /**
+     * @param $sql
+     *
+     * @return array
+     */
+    public function splitOld($sql)
+    {
+        $sql = $this->removeCommentsAndSpaces($sql);
+        return array_values(array_filter(explode(PHP_EOL . "" . PHP_EOL, $sql)));
     }
 
     /**
@@ -22,6 +93,7 @@ class QuerySplitter
      */
     private function removeCommentsAndSpaces($sql)
     {
+        $sql = preg_replace('@--.*?\n@mis', '', $sql);
         $sql = preg_replace('@\/\*.*?\*\/@mis', '', $sql);
         /* Trim spaces on blank lines */
         $sql = preg_replace('@^\s*\n@mis', "\n", $sql);
